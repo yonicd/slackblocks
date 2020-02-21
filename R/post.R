efd1fa65fd2753e5c2fbb923df7a5bfa24b4b35d
Jonathan Sidi <yonis@metrumrg.com>
2020-02-20 21:36:16 -0500

    refactor parse_link

--- R/post.R
+++ R/post.R
@@ -73,13 +73,20 @@ post.post_thread <- function(channel, block, token = Sys.getenv('SLACK_API_TOKEN
   invisible(res)
 }
 
-
 parse_link <- function(message_link){
   
-  link_attr <- strsplit(message_link,'\\?')[[1]][1]
-  thread_ts <- gsub('^p','',basename(link_attr))
-  thread_ts <- sprintf('%s.%s',substr(thread_ts,1,10),substr(thread_ts,11,16))
-  channel <- basename(dirname(link_attr))
+  split_message <- strsplit(message_link,'\\?')[[1]]
+  
+  if(length(split_message)==1){
+    link_attr <- split_message[1]
+    thread_ts <- gsub('^p','',basename(link_attr))
+    thread_ts <- sprintf('%s.%s',substr(thread_ts,1,10),substr(thread_ts,11,16))
+    channel <- basename(dirname(link_attr))  
+  }else{
+    link_attr <- strsplit(split_message[2],'\\&')[[1]]
+    thread_ts <- gsub('^(.*?)=','',link_attr[1])
+    channel <- gsub('^(.*?)=','',link_attr[2])
+  }
   
   list(thread_ts = thread_ts, channel = channel)
 }
@@ -140,3 +147,4 @@ prep_block <- function(block){
   
   block
 }
+
